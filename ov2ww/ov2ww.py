319b72bfe02bfded99e2685d47cb7a1c9f4b490b
Malcolm Harper <mharper@crcmail.net>
2022-08-17 13:52:30 -0400

    Move to functions, add no_accents flag

--- ov2ww/ov2ww.py
+++ ov2ww/ov2ww.py
@@ -1,54 +1,124 @@
 # !/usr/bin/env python3
+"""Produces a WeBWorK classlist from an Omnivox classlist.
 
+See --help for parameters
+"""
+
+from argparse import ArgumentParser
 import os.path
-import argparse
 import csv
 
 # Used to strip accents from names if the WeBWorK version can't handle accents
-import unidecode
+# import unidecode
 
-parser = argparse.ArgumentParser(
-    description = "Convert Omnivox classlist to WeBWorK classlist",
-)
+def main():
 
-parser.add_argument('input',
-    help = "input file from Omnivox\n student number, first and last name are required.  email address and section number are recommended.",
+    args = parse_args()
+
+
+
+    lang = args.lang
+
+    fields = definitions()
+
+    infile,outfile,processname = configure(args)
+
+    with open(infile, 'r') as csvin:
+        reader = csv.DictReader(csvin,delimiter=args.delimiter)
+        cols = reader.fieldnames
+        print(cols)
+
+        for c in ['student_id', 'last_name', 'first_name']:
+            if fields[c][lang] not in cols:
+                raise SystemExit(f"{fields[c][lang]} is a required field not found in input file '{infile}'\nNo output produced.")
+        for c in ['email_address', 'section']:
+            if fields[c][lang]  not in cols:
+                print(f"{fields[c][lang]} is a recommended field not found in {infile}")
+
+        with open(outfile, 'w', encoding='utf-8', newline='') as csvout:
+            writer = csv.writer(csvout,dialect='unix', quoting=csv.QUOTE_MINIMAL)
+            header_row = ["# Field order:", "student_id", "last_name", "first_name", "status", "comment", "section", "recitation", "email_address", "user_id", "password", "permission"]
+            writer.writerow(header_row)
+
+            translate = ['Student number']
+            for row in reader:
+            # Strip off the strange OV formatting '="...."'
+                row = {k:v[2:-1] if isinstance(v,str) and v.startswith('=') else v for (k,v) in row.items()}
+
+                classlist_row = [
+                    row[fields['student_id'][lang]],
+                    # unidecode.unidecode(row[fields['last_name'][lang]]),
+                    # unidecode.unidecode(row[fields['first_name'][lang]]),
+                    processname(row[fields['last_name'][lang]]),
+                    processname(row[fields['first_name'][lang]]),
+                    "C",
+                    "",
+                    row[fields['section'][lang]] if fields['section'][lang] in row and row[fields['section'][lang]] else '',
+                    "",
+                    row[fields['email_address'][lang]] if fields['email_address'][lang] in row and row[fields['email_address'][lang]] else '',
+                    row[fields['student_id'][lang]],
+                    "",
+                    0
+                ]
+
+                writer.writerow(classlist_row)
+
+def parse_args():
+
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('input',    help = "input file from Omnivox\n student number, first and last name are required.  email address and section number are recommended.",
     )
-
-parser.add_argument('--output', '-o',
+    parser.add_argument('--output', '-o',
     help = "output file name.  '.lst' will be appended if not already there."
     )
-
-parser.add_argument('--delimiter', '-d',
+    parser.add_argument('--delimiter', '-d',
     help = "Omnivox file delimiter",
     choices = [',',';'],
     default = ',',
     )
-
-parser.add_argument('--lang', '-l',
+    parser.add_argument('--lang', '-l',
     help = "Omnivox file language",
     choices = ['en','fr'],
     default = 'en',
     )
-
-parser.add_argument('--accents','-a',
+    parser.add_argument('--no_accents',
+    help = "Exclude accented characters from WW file\nRequires unidecode library",
+    default=False,
+    action="store_true",
     )
-args = parser.parse_args()
 
-infile = args.input
-if not os.path.isfile(infile):
-    raise SystemExit(f"Input file, {infile} not found")
+    return(parser.parse_args())
 
-outfile = args.output
-if outfile == None:
-    outfile = os.path.splitext(infile)[0]+'.lst'
-elif not os.path.splitext(outfile)[1] == '.lst':
-    outfile = outfile+'.lst'
+def configure(args):
 
-lang = args.lang
+    infile = args.input
+    if not os.path.isfile(infile):
+        raise SystemExit(f"Input file, {infile} not found.  Stopping.")
+
+    outfile = args.output
+    if outfile == None:
+        outfile = os.path.splitext(infile)[0]+'.lst'
+    elif not os.path.splitext(outfile)[1] == '.lst':
+        outfile = outfile+'.lst'
+    if args.no_accents:
+        try:
+            import unidecode # Used to strip accents from names if requested
+        except ImportError:
+            print("The module unidecode was not found. Accented characters will be retained")
+            args.no_accents = False
+    if args.no_accents:        
+        processname = noaccents
+    else:
+        processname = accents
+    
+    return infile, outfile, processname
+
+
+
+
 
 def definitions():
-    global fields 
+
     fields = {
         'student_id':{
             'en':'Student number',
@@ -71,43 +141,13 @@ def definitions():
             'fr':'Adresse de courriel'
         },
     }
-    
-definitions()
+    return(fields)
 
-with open(infile, 'r') as csvin:
-    reader = csv.DictReader(csvin,delimiter=args.delimiter)
-    cols = reader.fieldnames
-    print(cols)
+def accents(text):
+    return(text)
 
-    for c in ['student_id', 'last_name', 'first_name']:
-        if fields[c][lang] not in cols:
-             raise SystemExit(f"{fields[c][lang]} is a required field not found in input file '{infile}'\nNo output produced.")
-    for c in ['email_address', 'section']:
-        if fields[c][lang]  not in cols:
-            print(f"{fields[c][lang]} is a recommended field not found in {infile}")
+def noaccents(text):
+    return(unidecode.unidecode(text))
 
-    with open(outfile, 'w', encoding='utf-8', newline='') as csvout:
-        writer = csv.writer(csvout,dialect='unix', quoting=csv.QUOTE_MINIMAL)
-        header_row = ["# Field order:", "student_id", "last_name", "first_name", "status", "comment", "section", "recitation", "email_address", "user_id", "password", "permission"]
-        writer.writerow(header_row)
-
-        translate = ['Student number']
-        for row in reader:
-        # Strip off the strange OV formatting '="...."'
-            row = {k:v[2:-1] if isinstance(v,str) and v.startswith('=') else v for (k,v) in row.items()}
-
-            classlist_row = [
-                row[fields['student_id'][lang]],
-                unidecode.unidecode(row[fields['last_name'][lang]]),
-                unidecode.unidecode(row[fields['first_name'][lang]]),
-                "C",
-                "",
-                row[fields['section'][lang]] if fields['section'][lang] in row and row[fields['section'][lang]] else '',
-                "",
-                row[fields['email_address'][lang]] if fields['email_address'][lang] in row and row[fields['email_address'][lang]] else '',
-                row[fields['student_id'][lang]],
-                "",
-                0
-            ]
-
-            writer.writerow(classlist_row)
+if __name__ == '__main__':
+    main()
